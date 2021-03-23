28df09fb706eaad4d6814b34e9ad742d77579eda
akash121111 <akashkumar121111@gmail.com>
2021-03-23 08:08:30 -0400

    watchlist and watch history crud

---
+++ router/watchHistory.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const watchHistory = require('../model/watchHistory');
+const router = express.Router();
+
+//create
+router.post('/', (req, res) => {
+	const watchHistory = new watchHistory(req.body);
+	watchHistory
+		.save()
+		.then((data) => {
+			res.status(201).json({
+				ststus: 201,
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(400).json({
+				status: 400,
+				data: err
+			});
+		});
+});
+
+//retrieve
+router.get('/', (req, res) => {
+	watchHistory
+		.find()
+		.then((data) => {
+			res.status(200).json({
+				status: 200,
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(400).json({
+				status: 400,
+				data: err
+			});
+		});
+});
+
+//get by Id
+//retrieve
+router.get('/:id', (req, res) => {
+	var id = req.params.id;
+	watchHistory
+		.findById(id)
+		.then((data) => {
+			res.status(200).json({
+				status: 200,
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(404).json({
+				status: 404,
+				message: 'not found',
+				data: err
+			});
+		});
+});
+
+//update
+router.put('/:id', (req, res) => {
+	var id = req.params.id;
+	watchHistory
+		.findById(id)
+		.then((watchHistory) => {
+			watchHistory
+				.update(req.body)
+				.then((data) => {
+					res.status(201).json({
+						status: 201,
+						data: data,
+						message: 'updated successfully'
+					});
+				})
+				.catch((err) => {
+					res.status(400).json({
+						status: 400,
+						message: 'bad request',
+						data: err
+					});
+				});
+		})
+		.catch((err) => {
+			res.status(404).json({
+				status: 404,
+				message: 'not found',
+				data: err
+			});
+		});
+});
+
+//delete
+router.delete('/:id', (req, res) => {
+	var id = req.params.id;
+	watchHistory
+		.deleteOne({ id: id })
+		.then((data) => {
+			res.status(200).json({
+				status: 200,
+				message: 'deleted',
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(400).json({
+				status: 400,
+				message: 'not deleted'
+			});
+		});
+});
+
+module.exports = router;
