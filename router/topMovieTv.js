51245f0b353a74a06759a7446378a974d5ae60dd
akash121111 <akashkumar121111@gmail.com>
2021-03-23 08:20:49 -0400

    topMovieTv crud

---
+++ router/topMovieTv.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const TopMovieTv = require('../model/topMovieTv');
+const router = express.Router();
+
+//create
+router.post('/', (req, res) => {
+	const TopMovieTv = new TopMovieTv(req.body);
+	TopMovieTv.save()
+		.then((data) => {
+			res.status(201).json({
+				ststus: 201,
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(400).json({
+				status: 400,
+				data: err
+			});
+		});
+});
+
+//retrieve
+router.get('/', (req, res) => {
+	TopMovieTv.find()
+		.then((data) => {
+			res.status(200).json({
+				status: 200,
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(400).json({
+				status: 400,
+				data: err
+			});
+		});
+});
+
+//get by Id
+//retrieve
+router.get('/:id', (req, res) => {
+	var id = req.params.id;
+	TopMovieTv.findById(id)
+		.then((data) => {
+			res.status(200).json({
+				status: 200,
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(404).json({
+				status: 404,
+				message: 'not found',
+				data: err
+			});
+		});
+});
+
+//update
+router.put('/:id', (req, res) => {
+	var id = req.params.id;
+	TopMovieTv.findById(id)
+		.then((TopMovieTv) => {
+			TopMovieTv.update(req.body)
+				.then((data) => {
+					res.status(201).json({
+						status: 201,
+						data: data,
+						message: 'updated successfully'
+					});
+				})
+				.catch((err) => {
+					res.status(400).json({
+						status: 400,
+						message: 'bad request',
+						data: err
+					});
+				});
+		})
+		.catch((err) => {
+			res.status(404).json({
+				status: 404,
+				message: 'not found',
+				data: err
+			});
+		});
+});
+
+//delete
+router.delete('/:id', (req, res) => {
+	var id = req.params.id;
+	TopMovieTv.deleteOne({ id: id })
+		.then((data) => {
+			res.status(200).json({
+				status: 200,
+				message: 'deleted',
+				data: data
+			});
+		})
+		.catch((err) => {
+			res.status(400).json({
+				status: 400,
+				message: 'not deleted'
+			});
+		});
+});
+
+module.exports = router;
